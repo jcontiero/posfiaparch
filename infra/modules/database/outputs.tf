aad4fa3386cbbe6bb93909d949bd0f2eb128bd1e
Jonas Vasconcelos <jonascv@gmail.com>
2026-07-14 21:46:35 -0300

    fix: remove helm_release do postgres para evitar conflito com os manifestos

--- infra/modules/database/outputs.tf
+++ infra/modules/database/outputs.tf
@@ -1,20 +1,20 @@
 output "namespace" {
   description = "Namespace do PostgreSQL"
-  value       = helm_release.postgres.namespace
+  value       = kubernetes_namespace_v1.this.metadata[0].name
 }
 
 output "release_name" {
   description = "Nome do release Helm"
-  value       = helm_release.postgres.name
+  value       = var.release_name
 }
 
 output "service_name" {
   description = "Nome do service interno do PostgreSQL"
-  value       = "${helm_release.postgres.name}-postgresql"
+  value       = "postgres"
 }
 
 output "connection_string" {
   description = "String de conexão com o PostgreSQL (driver pg8000)"
-  value       = "postgresql+pg8000://${var.username}:${var.password}@${helm_release.postgres.name}-postgresql.${helm_release.postgres.namespace}.svc.cluster.local:5432/${var.database}"
+  value       = "postgresql+pg8000://${var.username}:${var.password}@postgres.${kubernetes_namespace_v1.this.metadata[0].name}.svc.cluster.local:5432/${var.database}"
   sensitive   = true
 }
